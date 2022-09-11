408cabf681fe025d278fe7c5d173db2f6ea851bd
Laboratory of Neurobiology <82390993+LaboratoryOfNeurobiology@users.noreply.github.com>
2022-09-11 17:00:32 -0400

    Add files via upload

--- vm_checks.py
+++ vm_checks.py
@@ -2,10 +2,10 @@ import numpy as np
 from pn_model import PacemakerNetwork
 from pn_model import simulate
 
-npc = 6
-nrc = 50
-npp = 1
-npr = 11
+npc = 86
+nrc = 20
+npp = 7
+npr = 7
 
 pars = dict(zip(['np', 'nr', 'npp', 'npr'], [npc, nrc, npp, npr]))
 dt, v0, sh = 143.0296, -60.6886, -10.2303
@@ -21,15 +21,6 @@ for i in range(len(net.traces)):
     v[i] = net.traces[i].volt.as_numpy()
 v = np.array(v).transpose()
 
-# =============================================================================
-# for i in range(len(net.traces)):
-#     ax = plt.axes(label = net.traces[i].name)
-#     j = (t > 0) & (t < 50)
-#     ax.plot(t[j], v[j, i])
-#     plt.title(net.traces[i].name)
-#     plt.show()
-# =============================================================================
-
 sso, nf, fm, fi, fs = net.freq_calc(cell_info = True)
 print(sso)
 
@@ -37,5 +28,5 @@ fs = np.hstack((nf[[0, 2, 3], np.newaxis], np.vstack((fm, fi, fs))))
 tv = np.concatenate((t[:, np.newaxis], v), axis = 1)
 fstv = np.vstack((fs, tv))
 
-f = '_'.join([str(p) for p in [npc, nrc, npp, npr]])
+f = '_'.join([str(p) for p in [sso, npc, nrc, npp, npr]])
 np.savetxt(f + '.csv', fstv, delimiter = ',')
